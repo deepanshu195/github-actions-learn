dcead0bc159130f899cffa336884b103ca1ae919
deepanshu195 <deepanshugarg195@gmail.com>
2023-12-14 12:10:59 +0530

    feat: env setup

--- init.js
+++ init.js
@@ -1,1 +1,2 @@
-console.log("Hello there date "+ new Date().getUTCDate());
+console.log("Hello there date " + new Date().getUTCDate());
+console.log("Node env, owner is = " + process.env.OWNER || "DEFAULT");
