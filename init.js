1e2e8097b18d5cde1de8178b96ba3350257ce5fe
deepanshu195 <deepanshugarg195@gmail.com>
2023-12-14 12:33:58 +0530

    feat: env tell

--- init.js
+++ init.js
@@ -1,3 +1,4 @@
 console.log("Hello there date " + new Date().getUTCDate());
 let owner = (process.env.OWNER == ""|| process.env.OWNER == undefined) ? "DEFAULT" : process.env.OWNER; 
 console.log("Node env, owner is = " + owner);
+console.log(process.env)
