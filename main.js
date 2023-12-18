9683b17fe3e100af71d25af745bcfcba4e1d555a
deepanshu195 <deepanshugarg195@gmail.com>
2023-12-18 14:52:55 +0530

    feat: js custom actions

---
+++ main.js
@@ -0,0 +1,10 @@
+
+const core = require("@actions/core");
+/**
+ * 
+ * The "  npm i @actions/core @actions/github @actions/exec --save " command is use for communicating message to github workflows.
+ * 
+ */
+
+core.notice("This is from core package to github actions output terminal");
+console.log("This is the main js running");
